fe11c7e4aa2025b59350b075244cd38073e965ae
Kostas Kilda <Kostas.Kilda@gmail.com>
2023-03-27 21:07:58 +0300

    Attempted cronjob fixes

--- watch.py
+++ watch.py
@@ -1,5 +1,9 @@
 import webbrowser
 import time
 
+print('\n\n------------------------------------------')
+print('Deploy watches')
+print('------------------------------------------\n\n')
+
 webbrowser.open('https://youtu.be/8Fske6WSqrs')
 time.sleep(600)
